382ae42887248bb0848982b8869e6e4f488183ac
rxblue <70175904+rxblue@users.noreply.github.com>
2020-10-27 20:07:22 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,17 @@ from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation, SecFeature
 from Bio.SeqRecord import SeqRecord
 import os.path
+
+filename = "/mnt/C/Users/OneDrive/Escritorio/ejercicio-biopython/biopython-notebook/notebooks/data/ls_orchid.gbk"
 def summarize_contents(filename):
-record=SEqIO.read(filename, "genbank")
-print("Name: ",record.name)
-print("Path: ",os.path.dirname(filename))
-record=list(SeqIO.parse(filename,"genbank"))
-print("Num_records:"%len(records))
-for seq_recors in SeqIO.parse(filename,"genbank"):
-print ("ID: ",record.id)
-print ("Name: ",record.name)
-print("Descriprion: ",record.description)
+lista = []
+lista = os.path.split(filename)
+record = list(SeqIO.parse(filename, "genbank"))
+cadena = ("file: " + lista[1])
+cadena += ("path" + lista[0])
+cadena += ("num_records = %i records" % len(records))
+for seq_record in SeqIO.parse(filename, "genbank"):
+    cadena += ("name: " + str(seq_record.name))
+    cadena += ("id: " + str(seq_record.id))
+    cadena += ("description: " + str(seq_record.description))
+print(cadena)
