877de58367a8d3850f73eb2d0da4f2e0928bd639
rxblue <70175904+rxblue@users.noreply.github.com>
2020-10-27 21:01:55 -0700

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,1 +1,22 @@
-
+import os
+import script
+import unittest
+class miPrueba(Unittest.testCase)
+  def test_summarize_contents(self)
+    s = script.summarize_contents(os.path.abspath("data/AF323668.gbk"))
+    self.assertDictEqual({""})
+    
+    s = script.summarize_contents(os.path.abspath("data/ls_orchid.fasta"))
+    self.assertDictEqual({""})
+    
+    s = script.summarize_contents(os.path.abspath("data/ls_orchid.gbk"))
+    self.assertDictEqual({""})
+    
+    s = script.summarize_contents(os.path.abspath("data/m_cold.fasta"))
+    self.assertDictEqual({""})
+    
+    s = script.summarize_contents(os.path.abspath("data/NC_002703.gbk"))
+    self.assertDictEqual({""})
+    
+    s = script.summarize_contents(os.path.abspath("data/opuntia.fasta"))
+    self.assertDictEqual({""})
