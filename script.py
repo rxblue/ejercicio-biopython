09333eb3bb5019a45ba1d93a88f3451db3f5b181
rxblue <rxbleu@outlook.com>
2020-11-09 20:10:23 -0700

    Este commit agrega una función para generar el complemento inverso de la concatenación de dos cadenas de ADN. Para esto se utiliza la librería de biopython [biopython-tarea3-ex1]

--- script.py
+++ script.py
@@ -1,19 +1,31 @@
 #Ejercicio biopython
 from Bio import SeqIO
-from Bio.SeqFeature import FeatureLocation, SecFeature
 from Bio.SeqRecord import SeqRecord
 import os.path
 
-filename = "/mnt/C/Users/OneDrive/Escritorio/ejercicio-biopython/biopython-notebook/notebooks/data/ls_orchid.gbk"
+filename = "/mnt/C/Users/rxblue/ejercicio-biopython/data/ls_orchid.gbk"
 def summarize_contents(filename):
-lista = []
-lista = os.path.split(filename)
-record = list(SeqIO.parse(filename, "genbank"))
-cadena = ("file: " + lista[1])
-cadena += ("path" + lista[0])
-cadena += ("num_records = %i records" % len(records))
-for seq_record in SeqIO.parse(filename, "genbank"):
-    cadena += ("name: " + str(seq_record.name))
-    cadena += ("id: " + str(seq_record.id))
-    cadena += ("description: " + str(seq_record.description))
-print(cadena)
+
+    lista = []
+    lista = os.path.split(filename)
+    records = list(SeqIO.parse(filename, "genbank"))
+    cadena = ("file: " + lista[1])
+    cadena += ("path" + lista[0])
+    cadena += ("num_records = %i records" % len(records))
+    for seq_record in SeqIO.parse(filename, "genbank"):
+        cadena += ("name: " + str(seq_record.name))
+        cadena += ("id: " + str(seq_record.id))
+        cadena += ("description: " + str(seq_record.description))
+    print(cadena)
+
+
+from Bio.Seq import Seq
+
+list_of_seqs = [Seq("acgtcgggtatacggcatgacgta"), Seq("gctgcagcagcattattacgcgcg")]
+def concatenate_and_get_reverse_of_complement():                                                       
+    concatenated = Seq("")
+    for s in list_of_seqs():
+        concatenated += s
+    concatenated
+    concatenated = my_seq
+    print(my_seq.reverse_complement())
